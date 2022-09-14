a7d2e371747c76f7d262f3dbe5827d8f912aadc4
Jakov Konta <JKonta@dspace.hr>
2022-09-14 23:42:38 +0200

    Setting up the project. Adding the first model in the App Library.

---
+++ SimpleKanBanLibrary/Models/StatusModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleKanBanLibrary.Models;
+
+public class StatusModel
+{
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string StatusId { get; set; }
+   public string StatusName { get; set; }
+   public string StatusDescription { get; set; }
+}
