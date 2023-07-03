936297128719b20d6ed0f05d7040a2c1b59f967d
jakovkonta <jako.konta@gmail.com>
2023-07-03 22:36:52 +0200

    added: status, category and item models

---
+++ SimpleKanBan/SimpleKanBanLibrary/Models/ItemModel.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleKanBanLibrary.Models;
+
+public class ItemModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+    public DateTime DateModified { get; set; }
+
+    public DateTime DateClosed { get; set; }
+
+    public string Author { get; set; }
+
+    public string Responsible { get; set; }
+
+    public StatusModel ItemStatus { get; set; }
+
+    public CategoryModel Category { get; set; }
+
+    public List<string> Comments { get; set; }
+
+    public bool Archived { get; set; }
+}
